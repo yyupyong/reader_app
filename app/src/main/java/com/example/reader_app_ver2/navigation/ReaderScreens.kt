72b09fb2a2e2f6eacbd08030056349b65e3dc562
yyupyong <yupyong.kim@gmail.com>
2023-05-28 07:12:38 +0900

    add enum screens

--- app/src/main/java/com/example/reader_app_ver2/navigation/ReaderScreens.kt
+++ app/src/main/java/com/example/reader_app_ver2/navigation/ReaderScreens.kt
@@ -1,4 +1,31 @@
 package com.example.reader_app_ver2.navigation
 
+import java.lang.IllegalArgumentException
+
 enum class ReaderScreens {
+    SplashScreen,
+    LoginScreen,
+    CreateAccountScreen,
+    ReaderHomeScreen,
+    SearchScreen,
+    DetailScreen,
+    UpdateScree,
+    ReaderStatsScreen;
+
+    companion object{
+        fun fromRoute(route:String):ReaderScreens
+        = when(route?.substringBefore("/")){
+            SplashScreen.name-> SplashScreen
+            LoginScreen.name-> LoginScreen
+            CreateAccountScreen.name-> CreateAccountScreen
+            ReaderHomeScreen.name-> ReaderHomeScreen
+            DetailScreen.name-> DetailScreen
+            SearchScreen.name-> SearchScreen
+            UpdateScree.name-> UpdateScree
+            ReaderStatsScreen.name-> ReaderStatsScreen
+            null-> ReaderHomeScreen
+            else -> throw IllegalArgumentException("Route $route is not recognized")
+        }
+    }
+
 }

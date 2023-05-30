ec59b1ffd16d9857e96c432e7b5ab814427e110f
yyupyong <yupyong.kim@gmail.com>
2023-05-30 10:02:26 +0900

    change navigation name

--- app/src/main/java/com/example/reader_app_ver2/screens/ReaderSplashScreen.kt
+++ app/src/main/java/com/example/reader_app_ver2/screens/ReaderSplashScreen.kt
@@ -1,7 +1,11 @@
 package com.example.reader_app_ver2.screens
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 
 @Composable
-fun ReaderSplashScreen() {
+fun ReaderSplashScreen(navController: NavController) {
+    Text(text = "SplashScreen")
 }

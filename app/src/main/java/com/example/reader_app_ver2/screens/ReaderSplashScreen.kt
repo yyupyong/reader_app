1f9cdf847bfa4966cbf1edff5f28a4d41f430d72
yyupyong <yupyong.kim@gmail.com>
2023-05-31 11:09:55 +0900

    画面起動時のエフェクトを追加

--- app/src/main/java/com/example/reader_app_ver2/screens/ReaderSplashScreen.kt
+++ app/src/main/java/com/example/reader_app_ver2/screens/ReaderSplashScreen.kt
@@ -1,11 +1,81 @@
 package com.example.reader_app_ver2.screens
 
+import android.content.Context
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import com.example.reader_app_ver2.R
+import com.example.reader_app_ver2.component.Center
+import com.example.reader_app_ver2.navigation.ReaderScreens
+import kotlinx.coroutines.delay
+import org.checkerframework.checker.units.qual.Current
+import org.w3c.dom.Text
+
+//@Preview
+//@Composable
+//fun ReaderSplashScreen(navController: NavController = NavController(context = LocalContext.current)) {
+//    Surface(
+//        modifier = Modifier
+//            .size(330.dp)
+//            .padding(15.dp),
+//        shape = CircleShape,
+//        border = BorderStroke(width = 2.dp, color = Color.LightGray),
+//    ) {
+//        Center {
+//            Text("検索中")
+//        }
+//    }
+//}
 
 @Composable
 fun ReaderSplashScreen(navController: NavController) {
-    Text(text = "SplashScreen")
+    val scale = remember {
+        androidx.compose.animation.core.Animatable(0f)
+    }
+
+    // AnimationEffect
+    LaunchedEffect(key1 = true) {
+        scale.animateTo(
+            targetValue = 0.7f,
+            animationSpec = tween(
+                durationMillis = 300,
+                easing = {
+                    OvershootInterpolator(4f).getInterpolation(it)
+                })
+        )
+        delay(3000L)
+        navController.navigate(ReaderScreens.SearchScreen.name)
+    }
+
+    // Image
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(text = "Splash")
+    }
 }
+
+

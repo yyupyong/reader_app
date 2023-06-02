6fd98df479a840d40144a4e503f642a8386263df
yyupyong <yupyong.kim@gmail.com>
2023-06-02 11:57:01 +0900

    add email input form

--- app/src/main/java/com/example/reader_app_ver2/screens/login/ReaderLoginScreen.kt
+++ app/src/main/java/com/example/reader_app_ver2/screens/login/ReaderLoginScreen.kt
@@ -1,18 +1,32 @@
 package com.example.reader_app_ver2.screens.login
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import com.example.reader_app_ver2.component.Center
@@ -27,22 +41,76 @@ fun ReaderLoginScreen(
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
-fun UserForm(){
-    val email  = rememberSaveable {
+fun UserForm() {
+    val email = rememberSaveable {
         mutableStateOf("")
     }
-    val password  = rememberSaveable {
+    val password = rememberSaveable {
         mutableStateOf("")
     }
     val passwordVisibility = rememberSaveable {
         mutableStateOf(false)
     }
     val passwordFocusRequest = FocusRequester.Default
+
     val keyboardController = LocalSoftwareKeyboardController.current
     //上記で設定したpasswordとemailがisNotEmptyだった場合trueをもつ
-    val valid = remember(email.value,password.value) {
+
+    val valid = remember(email.value, password.value) {
         email.value.trim().isNotEmpty() || password.value.trim().isNotEmpty()
     }
+
+    val modifier = Modifier
+        .height(250.dp)
+        .background(MaterialTheme.colorScheme.background)
+        .verticalScroll(
+            rememberScrollState()
+        )
+
+    //UserFormという大きなログイン専用のComposableがあって、ここで状態をホイスティングする
+    //子供としてColumnで各要素を持つ
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+
+
+    }
+
+}
+
+@Composable
+fun emailInput(
+    modifier: Modifier = Modifier,
+    emailState: MutableState<String>,
+    labelId: String = "Email",
+    enabled: Boolean = true,
+    imeAction: ImeAction = ImeAction.Next,
+    onKeyboardAction: KeyboardActions = KeyboardActions.Default
+) {
+    InputField()
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun InputField(
+    modifier: Modifier = Modifier,
+    valueState: MutableState<String>,
+    labelId: String,
+    enabled: Boolean,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    imeAction: ImeAction = ImeAction.Next,
+    onAction: KeyboardActions = KeyboardActions.Default,
+) {
+    OutlinedTextField(
+        value = valueState.value,
+        onValueChange = { valueState.value = it },
+        label = { Text(text = labelId) },
+        enabled = enabled,
+        keyboardOptions = KeyboardOptions(keyboardType = keyboardType, imeAction = imeAction),
+
+
+
+    )
 
 }

c3418f2fed2be8c619d61d7621192cc856f679eb
yyupyong <yupyong.kim@gmail.com>
2023-06-06 11:44:43 +0900

    add authenticationLogic in LoginViewModel and make MutableLiveDataState

--- app/src/main/java/com/example/reader_app_ver2/screens/login/LoginScreenViewModel.kt
+++ app/src/main/java/com/example/reader_app_ver2/screens/login/LoginScreenViewModel.kt
@@ -1,2 +1,32 @@
 package com.example.reader_app_ver2.screens.login
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.flow.MutableStateFlow
+
+class LoginScreenViewModel : ViewModel() {
+
+//    val loadingState = MutableStateFlow(LoadingState.IDLE)
+    private val auth: FirebaseAuth = Firebase.auth
+
+    private val _loading = MutableLiveData(false)
+    val loading: LiveData<Boolean> = _loading
+
+
+    fun signInWithEmailAndPassword(email: String, password: String) {
+        try {
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    //成功時の処理を記述
+                }
+            }
+        } catch (e: Exception) {
+            //Log.d(TAG, "signInWithEmailAndPassword: ")
+        }
+    }
+}

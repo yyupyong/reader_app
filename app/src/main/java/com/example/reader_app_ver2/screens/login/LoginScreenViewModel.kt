152406c395dca6a1ccd6e330fdc5f9c3b82d1d8b
yyupyong <yupyong.kim@gmail.com>
2023-06-08 09:32:41 +0900

    add createAccount Logic

--- app/src/main/java/com/example/reader_app_ver2/screens/login/LoginScreenViewModel.kt
+++ app/src/main/java/com/example/reader_app_ver2/screens/login/LoginScreenViewModel.kt
@@ -4,10 +4,12 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 
 class LoginScreenViewModel : ViewModel() {
 
@@ -19,7 +21,7 @@ class LoginScreenViewModel : ViewModel() {
 
 
 
-    fun signInWithEmailAndPassword(email: String, password: String, home:()->Unit) {
+    fun signInWithEmailAndPassword(email: String, password: String, home:()->Unit) = viewModelScope.launch {
         try {
             auth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
                 if (it.isSuccessful) {
@@ -30,5 +32,26 @@ class LoginScreenViewModel : ViewModel() {
         } catch (e: Exception) {
             //Log.d(TAG, "signInWithEmailAndPassword: ")
         }
+    }
+
+
+
+    fun createUserWithEmailAndPassword (
+        email: String,
+        password: String,
+        home: () -> Unit) = viewModelScope.launch{
+            if(_loading.value == false) {
+                _loading.value = true
+                auth.createUserWithEmailAndPassword(email, password)
+                    //処理が完了したかどうかtask.isSuccessは成功かどうか
+                    .addOnCompleteListener{ task->
+                        if (task.isSuccessful){
+                            home()
+                        }else{
+                            Log.d("FB", "currentUserEmailAndPassword${task.result}")
+                        }
+                        _loading.value == false
+                    }
+            }
     }
 }

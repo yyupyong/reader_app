208cbd28a653d9245a421fce6706b07a023f4282
yyupyong <yupyong.kim@gmail.com>
2023-06-08 09:57:09 +0900

    change MutableLive to StateFlow

--- app/src/main/java/com/example/reader_app_ver2/screens/login/LoginScreenViewModel.kt
+++ app/src/main/java/com/example/reader_app_ver2/screens/login/LoginScreenViewModel.kt
@@ -9,49 +9,51 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import java.util.Stack
 
 class LoginScreenViewModel : ViewModel() {
 
-//    val loadingState = MutableStateFlow(LoadingState.IDLE)
+    //    val loadingState = MutableStateFlow(LoadingState.IDLE)
     private val auth: FirebaseAuth = Firebase.auth
 
-    private val _loading = MutableLiveData(false)
-    val loading: LiveData<Boolean> = _loading
+    private val _loading = MutableStateFlow(false)
+    val loading: StateFlow<Boolean> = _loading
 
 
-
-    fun signInWithEmailAndPassword(email: String, password: String, home:()->Unit) = viewModelScope.launch {
-        try {
-            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
-                if (it.isSuccessful) {
-                    //成功時の処理を記述
-                    home()
+    fun signInWithEmailAndPassword(email: String, password: String, home: () -> Unit) =
+        viewModelScope.launch {
+            try {
+                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        //成功時の処理を記述
+                        home()
+                    }
                 }
+            } catch (e: Exception) {
+                //Log.d(TAG, "signInWithEmailAndPassword: ")
             }
-        } catch (e: Exception) {
-            //Log.d(TAG, "signInWithEmailAndPassword: ")
         }
-    }
 
 
-
-    fun createUserWithEmailAndPassword (
+    fun createUserWithEmailAndPassword(
         email: String,
         password: String,
-        home: () -> Unit) = viewModelScope.launch{
-            if(_loading.value == false) {
-                _loading.value = true
-                auth.createUserWithEmailAndPassword(email, password)
-                    //処理が完了したかどうかtask.isSuccessは成功かどうか
-                    .addOnCompleteListener{ task->
-                        if (task.isSuccessful){
-                            home()
-                        }else{
-                            Log.d("FB", "currentUserEmailAndPassword${task.result}")
-                        }
-                        _loading.value == false
+        home: () -> Unit
+    ) = viewModelScope.launch {
+        if (!_loading.value) {
+            _loading.value = true
+            auth.createUserWithEmailAndPassword(email, password)
+                //処理が完了したかどうかtask.isSuccessは成功かどうか
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        home()
+                    } else {
+                        Log.d("FB", "currentUserEmailAndPassword${task.result}")
                     }
-            }
+                    !_loading.value
+                }
+        }
     }
 }

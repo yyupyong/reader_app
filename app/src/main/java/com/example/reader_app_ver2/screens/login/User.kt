32bc4b11f42f57d319582c944140e7e308e9d4de
yyupyong <yupyong.kim@gmail.com>
2023-06-11 15:51:49 +0900

    create user Model

--- app/src/main/java/com/example/reader_app_ver2/screens/login/User.kt
+++ app/src/main/java/com/example/reader_app_ver2/screens/login/User.kt
@@ -1,4 +1,20 @@
 package com.example.reader_app_ver2.screens.login
 
-class User {
+class User(
+    val id: String?,
+    val userId: String,
+    val displayName: String,
+    val avatarUrl: String,
+    val quote: String,
+    val profession: String,
+) {
+    fun toMap(): MutableMap<String, Any> {
+        return mutableMapOf(
+            "user_id" to this.userId,
+            "displayName" to this.displayName,
+            "avatarUrl" to this.avatarUrl,
+            "quote" to this.quote,
+            "profession" to this.profession,
+        )
+    }
 }

95ccd440ed387681986da48a56949a3b1055ff33
yyupyong <yupyong.kim@gmail.com>
2023-06-12 21:45:13 +0900

    add FAB composable

--- app/src/main/java/com/example/reader_app_ver2/screens/home/ReaderHomeScreen.kt
+++ app/src/main/java/com/example/reader_app_ver2/screens/home/ReaderHomeScreen.kt
@@ -1,27 +1,51 @@
 package com.example.reader_app_ver2.screens.home
 
+import android.annotation.SuppressLint
+import android.net.wifi.hotspot2.pps.HomeSp
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.reader_app_ver2.component.Center
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ReaderHomeScreen() {
-    Center(
-        modifier = Modifier.fillMaxSize(),
-    ) {
-        Text(text = "Home")
+fun Home() {
+    Scaffold(topBar = {}, floatingActionButton = { FABContent{}}) {
+        Surface(modifier = Modifier.fillMaxSize()) {
+
+        }
     }
 }
+
+@Composable
+fun FABContent(onTap: (String) -> Unit) {
+    FloatingActionButton(
+        onClick = { onTap("") },
+        shape = RoundedCornerShape(50.dp),
+        containerColor = MaterialTheme.colorScheme.background,
+    ) {
+        Icon(imageVector = Icons.Default.Add, contentDescription = "")
+    }
+}
+
 
